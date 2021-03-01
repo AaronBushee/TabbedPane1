563253c96a78abbdcc0d22194644256182321387
Aaron Bushee <91086994@mvusd.net>
2021-03-01 19:19:03 +0000

    Initial commit

---
+++ JTabbedPaneFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class JTabbedPaneFrame extends JFrame
+{
+  public JTabbedPaneFrame()
+  {
+    super("JTabbedPane Demo");
+
+  }//end default and only contructor
+  
+}//end class
